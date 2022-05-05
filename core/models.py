4b16b42d13e6aecceedf7eee1bcbfe8c275ad058
kiarashfz <kiawfz3673@gmail.com>
2022-05-06 02:32:48 +0430

    BaseModel & BaseUser moved to core app models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    class Meta:
+        abstract = True
+
+    created = models.DateTimeField(
+        auto_now_add=True,
+        editable=False,
+    )
+
+    last_updated = models.DateTimeField(
+        auto_now=True,
+        editable=False,
+    )
+
+
+class User(AbstractUser):
+    pass
